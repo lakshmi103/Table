5018daa0282d1bfa7a1f203610af16680509efd2
Austin Benitez <austin.i.davis94@gmail.com>
2021-08-12 04:04:58 -0700

    Defined how to get a row

--- table.js
+++ table.js
@@ -15,8 +15,21 @@ fetch('http://127.0.0.1:5500/table.json').then(
 
 // get the div that will hold the table
 fucntion getTableElement(){
-    tableBody = document.getElementsByClassName("table-body")
+    var tableBody = document.getElementsByClassName("table-body")
     return tableBody
 }
 
-function generateRow
+function generateRow(data){
+    // We create an empty row
+    var rowContainer = document.createElement("tr")
+    // TODO: This needs to be changed to fit the incoming data layout but the internals are relativly
+    // the same
+    for(item in data){
+        // Create new row data element
+        let rowData = document.createElement("td")
+        // set that elements text to the appropriate item
+        rowData.innerHTML = item
+        // add that data to the row
+        rowContainer.appendChild(rowData)
+    }
+}
