b09de91afb80faaf65f558daf74ffd63aad28d23
Austin Benitez <austin.i.davis94@gmail.com>
2021-08-12 05:28:37 -0700

    Functional Javascript creates the table

--- table.js
+++ table.js
@@ -21,60 +21,65 @@ function printElement(element){
 }
 
 // get the div that will hold the table
-function getTableElement()
+function getTableBodyElement()
 {
     var tableBody = document.getElementsByClassName("table-body")
     return tableBody
 }
 // Get the head container
-function getTableElement()
+function getTableHeadElement()
 {
     var tableBody = document.getElementsByClassName("table-head")
     return tableBody
 }
-
-function generateRow(data){
-    // We create an empty row
-    var rowContainer = document.createElement("tr")
-    // TODO: This needs to be changed to fit the incoming data layout but the internals are relativly
-    // the same
-    for(item in data){
-        console.log(item)
-        // Create new row data element
-        let rowData = document.createElement("td")
-        // set that elements text to the appropriate item
-        rowData.innerHTML = item
-        // add that data to the row
-        rowContainer.appendChild(rowData)
-    }
-    return rowContainer;
+// Get the table container
+function getTableElement()
+{
+    var tableBody = document.getElementById("table")
+    return tableBody
 }
 
-function generateHeader(data){
+function generateCellData(element, row){
+    console.log(element)
+    // Create new row data element
+    // set that elements text to the appropriate item
+    cell = row.insertCell();
+    cell.innerHTML = element
+}
+
+function generateHeader(data, table){
     // We create an empty row
-    var rowContainer = document.createElement("tr")
+    var header = table.createTHead();
+    var row = header.insertRow();
+
     // TODO: This needs to be changed to fit the incoming data layout but the internals are relativly
     // the same
-    for(item in data){
-        console.log(item)
-        // Create new row data element
-        let rowData = document.createElement("th")
-        // set that elements text to the appropriate item
-        rowData.innerHTML = item
-        // add that data to the row
-        rowContainer.appendChild(rowData)
-    }
+    data.forEach(element=> generateCellData(element, row))
+}
+
+function generateRow(data, table){
+    // We create an empty row
+    var row = table.insertRow();
+
+    // TODO: This needs to be changed to fit the incoming data layout but the internals are relativly
+    // the same
+    data.forEach(element=> generateCellData(element, row))
 }
 
 function generateTable(row){
+    let table = getTableElement();
     // build header
     if (first == 0){
         console.log(row)
         first = 1
+        generateHeader(row, table)
+    //get header from doc
+    //add header to doc
+    // header.appendChild(headerData);
     }
     // build rows
     else{
-        first = 13;
+        generateRow(row, table)
     }
 
 }
